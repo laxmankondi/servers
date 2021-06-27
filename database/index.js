1bdb85ef18e25ab537db07252a2cf2af35e33910
laxmankondi <laxmankondi.v@gmail.com>
2021-06-27 16:56:54 +0530

    put post

--- database/index.js
+++ database/index.js
@@ -1,48 +1,45 @@
-const books = [{
-    ISBN: "1234One",
-    title: "Getting started with MERN",
-    authors: [1, 2],
-    language : "en",
-    pubDate: "2021-07-07",
-    numOfPage: 255,
-    category: ["fiction", "programming", "tech", "web dev"],
-    publications: 1,
+const books = [
+    {
+      ISBN: "12345ONE",
+      title: "Getting started with MERN",
+      authors: [1, 2],
+      language: "en",
+      pubDate: "2021-07-07",
+      numOfPage: 225,
+      category: ["fiction", "programming", "tech", "web dev"],
+      publication: 1,
     },
     {
-    ISBN: "1234Two",
-    title: "Getting started with python",
-    authors: [1, 2],
-    language : "en",
-    pubDate: "2021-07-07",
-    numOfPage: 255,
-    category: ["fiction", "programming", "tech", "web dev"],
-    publications: 1,
-
-
-},
-];
-
-const authors = [
+      ISBN: "12345Two",
+      title: "Getting started with Python",
+      authors: [1, 2],
+      language: "en",
+      pubDate: "2021-07-07",
+      numOfPage: 225,
+      category: ["fiction", "tech", "web dev"],
+      publication: 1,
+    },
+  ];
+  const authors = [
     {
-        id: 1,
-        name: "laxman",
-        books: ["12345ONE"],
+      id: 1,
+      name: "pavan",
+      books: ["12345ONE", "12345Two"],
     },
     {
-        id: 2,
-        name: "Ramu",
-        books: ["12345ONE"],
+      id: 2,
+      name: "Deepak",
+      books: ["12345ONE"],
+    },
+  ];
+  const publications = [
+    {
+      id: 1,
+      name: "Chakra",
+      books: ["12345ONE"],
+    },
+  ];
   
-      },
-];
-
-const publications = [
-    {
-        id: 1,
-        name: "Chakra",
-        books: ["12345ONE"],
-    },
-    
-];
-
-module.exports = {books, authors, publications };
+  module.exports = { books, authors, publications };
+  
+  
